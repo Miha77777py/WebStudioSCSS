00b4ef74777c4d47cdc4e7665e29ae1fa5d9205d
Miha77777 <lv11miha@gmail.com>
2024-02-17 23:49:44 +0200

    done portfolio scss

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       backdrop: document.querySelector("[data-backdrop]"),
+      modal: document.querySelector("[data-modal]")
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +11,6 @@
   
     function toggleModal() {
       refs.backdrop.classList.toggle("is-hidden");
+      refs.modal.classList.toggle("on-animate")
     }
 })();
